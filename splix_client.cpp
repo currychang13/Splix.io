3632072b62da84e41c8718868f3b6ea0b087f02f
= <=>
2024-12-28 23:44:25 +0800

    select room complete

--- splix_client.cpp
+++ splix_client.cpp
@@ -1,9 +1,9 @@
 #include "splix_header.h"
-
 int map[MAP_HEIGHT][MAP_WIDTH];
 enum class GameStatus
 {
     INITIAL,
+    ROOM_SELECTION,
     GAMING,
     GAME_OVER
 };
@@ -46,6 +46,7 @@ void game_loop(Splix_Window *game_win, Status_Window *stat_win)
     {
         if ((ch = wgetch(game_win->win)) != ERR)
         {
+            std::pair<int, int> new_direction = direction;
             switch (ch)
             {
             case 'q':
@@ -54,32 +55,30 @@ void game_loop(Splix_Window *game_win, Status_Window *stat_win)
                 return; // exit the function
             case 'w':
             case KEY_UP:
-                direction.first = -1;
-                direction.second = 0;
+                new_direction = {-1, 0};
                 break; // break means exit the switch, not the loop,which ignores the operation
             case 's':
             case KEY_DOWN:
-
-                direction.first = 1;
-                direction.second = 0;
+                new_direction = {1, 0};
                 break;
             case 'a':
             case KEY_LEFT:
-
-                direction.first = 0;
-                direction.second = -1;
+                new_direction = {0, -1};
                 break;
             case 'd':
             case KEY_RIGHT:
-                direction.first = 0;
-                direction.second = 1;
+                new_direction = {0, 1};
                 break;
             default:
                 break;
             }
+            if (new_direction.first != -direction.first && new_direction.second != -direction.second)
+            {
+                direction = new_direction;
+            }
         }
-        // update position
 
+        // update position
         coordinate_y += direction.first;
         coordinate_x += direction.second;
 
@@ -108,6 +107,55 @@ void game_loop(Splix_Window *game_win, Status_Window *stat_win)
         usleep(200000); // Sleep for 0.1sec, speed of the game
     }
 }
+
+int connect_to_server()
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Initialize server address
+    bzero(&servaddr, sizeof(servaddr));                       // Clear struct
+    servaddr.sin_family = AF_INET;                            // IPv4
+    servaddr.sin_port = 12345;                                // Server port (convert to network byte order)
+    inet_pton(AF_INET, "140.113.66.205", &servaddr.sin_addr); // Convert IP address to binary form
+
+    // Connect to server
+    connect(sockfd, (const sockaddr *)&servaddr, sizeof(servaddr));
+
+    return sockfd;
+}
+void send_server_name(int sockfd, const char *name)
+{
+    // Send name to server
+    write(sockfd, (void *)name, strlen(name));
+}
+std::vector<std::pair<int, int>> receive_room_info(int sockfd)
+{
+    // Receive room info from server
+    char room_number[100];
+    char room_str[100];
+    std::vector<std::pair<int, int>> room_info;
+    read(sockfd, room_number, sizeof(room_number));
+    int room_num = atoi(room_number);
+    for (int i = 0; i < room_num; i++)
+    {
+        int room_id, room_player;
+        read(sockfd, room_str, sizeof(room_str));
+        sscanf(room_str, "%d %d", &room_id, &room_player);
+        room_info.push_back({room_id, room_player});
+    }
+    return room_info;
+}
+void send_server_room(int sockfd, int room_id)
+{
+    // Send room id to server
+    char room_str[10];
+    sprintf(room_str, "%d", room_id);
+    write(sockfd, (void *)room_str, strlen(room_str));
+}
 int main()
 {
     // signal(SIGWINCH, SIG_IGN); /* ignore window size changes */
@@ -126,18 +174,38 @@ int main()
     while (true)
     {
         Initial_Window init_win(HEIGHT_INIT_WIN, WIDTH_INIT_WIN, (LINES - HEIGHT_INIT_WIN) / 2, (COLS - WIDTH_INIT_WIN) / 2);
+        Room_Window room_win(HEIGHT_INIT_WIN, WIDTH_INIT_WIN, (LINES - HEIGHT_INIT_WIN) / 2, (COLS - WIDTH_INIT_WIN) / 2);
         Input_Window input_win(HEIGHT_INIT_WIN / 12, WIDTH_GAME_WIN / 2, (HEIGHT_INIT_WIN - HEIGHT_GAME_WIN / 12) / 2.5, (COLS - WIDTH_GAME_WIN / 2) / 2);
         Status_Window stat_win(HEIGHT_GAME_WIN / 10, WIDTH_GAME_WIN / 4, (LINES - HEIGHT_GAME_WIN) / 2, (COLS + WIDTH_GAME_WIN) / 2);
         Splix_Window splix_win(HEIGHT_GAME_WIN, WIDTH_GAME_WIN, (LINES - HEIGHT_GAME_WIN) / 2, (COLS - WIDTH_GAME_WIN) / 2);
+
+        // int sockfd = connect_to_server();
+        // send_server_name(sockfd, input_win.name);
+        std::vector<std::pair<int, int>> room_info;
+        room_info.push_back({1, 2});
+        room_info.push_back({2, 3});
         switch (status)
         {
         case GameStatus::INITIAL:
             init_win.draw();
             init_win.Rendertitle();
-            // clear();
-            // wrefresh(stdscr);
             input_win.draw();
             input_win.get_user_input();
+            // send name to server
+            // send_server_name(sockfd, input_win.name);
+            // room_info = receive_room_info(sockfd);
+            status = GameStatus::ROOM_SELECTION;
+            break;
+        case GameStatus::ROOM_SELECTION:
+            room_win.draw();
+            room_win.Renderroom();
+            room_win.select_room(room_info);
+            if (room_win.quit)
+            {
+                status = GameStatus::INITIAL;
+                break;
+            }
+            //send_server_room(sockfd, room_info[room_win.selected_room].first);
             status = GameStatus::GAMING;
             break;
         case GameStatus::GAMING:
